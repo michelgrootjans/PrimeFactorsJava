6353c041ffe23bc13aadc3ecabadae397ade620e
Michel Grootjans <michel.grootjans@gmail.com>
2015-12-11 10:51:33 +0100

    starting state for kata

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -4,17 +4,4 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-
-    public List<Integer> generate (int number) {
-        List<Integer> factorsList = new ArrayList<Integer>();
-        int half = 2;
-        while (number > 1) {
-            while (number % half == 0) {
-                factorsList.add(half);
-                number /= half;
-            }
-            half++;
-        }
-        return factorsList;
-    }
 }
